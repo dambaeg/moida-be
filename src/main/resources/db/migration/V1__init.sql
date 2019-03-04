c21f4e58179a26ce804d3dd7735935f4b099d92e
brainbackdoor <brainbackdoor@gmail.com>
2019-03-04 10:41:22 +0900

    fix(sql): properties

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,26 +1,33 @@
 CREATE TABLE member
 (
   id VARCHAR(8) not null,
+  created_date datetime,
+  updated_date datetime,
   name VARCHAR(255) NOT NULL,
-  blogLink VARCHAR(255)	NOT NULL,
+  blog_link VARCHAR(255)	NOT NULL,
   primary key (id)
-);
+) engine=InnoDB;
+
 CREATE TABLE post
 (
   id VARCHAR(8) not null,
+  created_date datetime,
+  updated_date datetime,
   member_id VARCHAR(8) NOT NULL,
   title VARCHAR(255) NOT NULL,
-  description TEXT	NOT NULL
+  description TEXT	NOT NULL,
   primary key (id)
-);
+) engine=InnoDB;
 
 CREATE TABLE comment
 (
   id bigint not null,
-  post_id BIGINT NOT NULL,
+  created_date datetime,
+  updated_date datetime,
+  post_id VARCHAR(8)  NOT NULL,
   content VARCHAR(255)	NOT NULL,
   primary key (id)
-);
+) engine=InnoDB;
 
 alter table post
    add constraint fk_post_to_member
@@ -29,5 +36,5 @@ alter table post
 
 alter table comment
    add constraint fk_comment_to_post
-   foreign key (member_id)
-   references member (id);
+   foreign key (post_id)
+   references post (id);
